32e942234e664773bd0d444b02e99299787005e7
Megan Fayery and Ronando Long <student@epicodus.com>
2016-01-19 10:09:12 -0800

    add initial files, folders, anf functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
-// var helloWorld = function(){
-//   return false;
-// };
+// Business Logic
+var leapYear = function(year) {
+  console.log(year);
+  if (year === "") {  //(year === "")
+    return "Please just enter a number bitch!";
+  } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else if (isNaN(year)) {
+    return "Please just enter a number bitch!";
+  } else if (year <=0) {
+    return "input positive integer";
+  } else {
+    return false;
+  }
+};
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#yourYear").submit(function(event) {
+    
+
+    $(".results").show();
+
+    event.preventDefault();
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,31 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe('leapYear', function() {
+
+  it("is false for a year that is not divisible by 4, 100 and 400", function(){
+    expect(leapYear(1973)).to.equal(false);
+  });
+
+  it("it is true for a year that is divisible by 4", function(){
+    expect(leapYear(2012)).to.equal(true);
+  });
+
+  it("it is false for a year that is divisible by 100", function(){
+    expect(leapYear(1900)).to.equal(false);
+  });
+
+  it("it is true for a year that is divible by 400", function(){
+    expect(leapYear(2000)).to.equal(true);
+  });
+
+  it("it returns message in browser encouraging user to enter positive number only", function(){
+    expect(leapYear("hello")).to.equal("Please just enter a number bitch!");
+  });
+
+  it("it returns message prompting integer when user inputs negative number", function(){
+    expect(leapYear(-45)).to.equal("input positive integer");
+  });
+
+  it("it returns message prompting integer when user enters empty value", function(){
+    expect(leapYear("")).to.equal("Please just enter a number bitch!");
+  });
+
+});
